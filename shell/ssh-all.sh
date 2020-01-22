c59c1aa3789237f01a6168c1bee2b6b571012c0d
cisdi-huang <cqupt_hm@163.com>
2020-01-22 14:36:17 +0800

    feat(shell): 更新免密登录脚本

--- shell/ssh-all.sh
+++ shell/ssh-all.sh
@@ -17,22 +17,26 @@ filename=$1
 
 # 所有节点修改hosts文件，增加host映射
 cat $filename | while read ip1 host1 sort1 pwd1; do
-  sshpass -p $pwd1 ssh root@$ip1 "hostnamectl set-hostname $host1"
+  ssh -f -o StrictHostKeyChecking=no root:$pwd1$@$ip1 "hostnamectl set-hostname $host1"
   cat $filename | while read ip2 host2 sort2 pwd2; do
-    sshpass -p $pwd1 ssh root@$ip1 "echo $ip2 $host2 $sort2 >> /etc/hosts"
+    ssh -f -o StrictHostKeyChecking=no root:$pwd1@$ip1 "echo $ip2 $host2 $sort2 >> /etc/hosts"
   done
 done
+tput setaf 3
 echo "=====主机名、host文件配置完成！(Host name, host file configuration is complete!)====="
+tput setaf 7
 
 # 所有节点生成秘钥文件(Generate a key file for all nodes)
 cat $filename | while read ip host sort pwd; do
-  sshpass -p $pwd ssh root@$ip "ssh-keygen -t rsa -P '' -f ~/.ssh/id_rsa"
+  ssh -f -o StrictHostKeyChecking=no root:$pwd@$ip "ssh-keygen -t rsa -P '' -f ~/.ssh/id_rsa"
 done
 
 # 将每个节点的id拷贝到所有节点的authorized_keys文件中(Copy the id of each node into the authorized_keys file of all nodes)
 cat $filename | while read ip1 host1 sort1 pwd1; do
   cat $filename | while read ip2 host2 sort2 pwd2; do
-    sshpass -p $pwd1 ssh -o StrictHostKeyChecking=no root@$ip1 "sshpass -p $pwd2 ssh-copy-id -f $ip2 2>/dev/null"
+    ssh -f -o StrictHostKeyChecking=no root@$ip1 "sshpass -p $pwd2 ssh-copy-id -f $ip2 2>/dev/null"
   done
 done
+tput setaf 3
 echo "=====免密登录完成！(Secret-free login is complete!)====="
+tput setaf 7
