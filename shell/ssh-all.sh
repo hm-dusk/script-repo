5d8ac0de58a21edd2c4877e09569f87401c7fad6
13222 <1322260665@qq.com>
2020-02-27 11:33:20 +0800

    feat(shell脚本): 更新脚本
    1、新增离线yum源配置脚本
    2、更新一键免密登录脚本

--- shell/ssh-all.sh
+++ shell/ssh-all.sh
@@ -11,31 +11,80 @@
 # [ip2] [hostname2] [sort hostname2] [password2]
 # ###
 
-# 获取数据文件名
+# 获取数据文件名（Get data file name）
 filename=$1
 
-# 所有节点修改hosts文件，增加host映射
+# 所有节点修改主机名、hosts文件（Modify the host name and hosts file for all nodes）
 cat ${filename} | while read ip1 host1 sort1 pwd1; do
-  ssh -f -o StrictHostKeyChecking=no root:${pwd1}$@${ip1} "hostnamectl set-hostname $host1"
+expect << EOF
+  spawn ssh root@${ip1} "hostnamectl set-hostname ${host1}"
+  expect {
+  "yes/no" {send "yes\r"; exp_continue}
+  "password" {send "${pwd1}\r"}
+  }
+  expect eof
+EOF
+# 在当前节点hosts文件中增加host映射（Add host mapping in hosts file of current node）
   cat ${filename} | while read ip2 host2 sort2 pwd2; do
-    ssh -f -o StrictHostKeyChecking=no root:${pwd1}@${ip1} "echo $ip2 $host2 $sort2 >> /etc/hosts"
+expect << EOF
+  spawn ssh root@${ip1} "echo ${ip2} ${host2} ${sort2} >> /etc/hosts"
+  expect {
+  "yes/no" {send "yes\r"; exp_continue}
+  "password" {send "${pwd1}\r"}
+  }
+  expect eof
+EOF
   done
 done
+
 tput setaf 3
 echo "=====主机名、host文件配置完成！(Host name, host file configuration is complete!)====="
 tput setaf 7
 
 # 所有节点生成秘钥文件(Generate a key file for all nodes)
 cat ${filename} | while read ip host sort pwd; do
-  ssh -f -o StrictHostKeyChecking=no root:${pwd}@${ip} "ssh-keygen -t rsa -P '' -f ~/.ssh/id_rsa"
+expect << EOF
+  spawn ssh root@${sort} "ssh-keygen -t rsa -P '' -f ~/.ssh/id_rsa"
+  expect {
+  "yes/no" {send "yes\r"; exp_continue}
+  "password" {send "${pwd}\r"}
+  }
+  expect eof
+EOF
 done
 
-# 将每个节点的id拷贝到所有节点的authorized_keys文件中(Copy the id of each node into the authorized_keys file of all nodes)
-cat ${filename} | while read ip1 host1 sort1 pwd1; do
-  cat ${filename} | while read ip2 host2 sort2 pwd2; do
-    ssh -f -o StrictHostKeyChecking=no root@${ip1} "sshpass -p $pwd2 ssh-copy-id -f $ip2 2>/dev/null"
-  done
+# 将所有节点的key文件内容添加到本地临时authorized_keys文件
+touch ./authorized_keys
+cat ${filename} | while read ip host sort pwd; do
+# 复制节点的key文件到本地
+expect << EOF
+  spawn scp ${sort}:/root/.ssh/id_rsa.pub ./${sort}.pub
+  expect {
+  "yes/no" {send "yes\r"; exp_continue}
+  "password" {send "${pwd}\r"}
+  }
+  expect eof
+EOF
+# 将key文件内容添加到本地临时authorized_keys文件中
+cat ./${sort}.pub >> ./authorized_keys
+# 删除临时key文件
+rm -f ./${sort}.pub
 done
+
+# 将组装好的authorized_keys文件分发到所有节点
+cat ${filename} | while read ip host sort pwd; do
+expect << EOF
+  spawn scp ./authorized_keys ${sort}:/root/.ssh/
+  expect {
+  "yes/no" {send "yes\r"; exp_continue}
+  "password" {send "${pwd}\r"}
+  }
+  expect eof
+EOF
+done
+# 删除临时authorized_keys文件
+rm -f ./authorized_keys
+
 tput setaf 3
 echo "=====免密登录完成！(Secret-free login is complete!)====="
 tput setaf 7
---
+++ shell/yum-repo.sh
@@ -0,0 +1,62 @@
+#! /bin/bash
+
+# ###
+# 配置离线yum源
+# author：hm
+# version: 1.0
+# use: xxx.sh xxx.iso 192.168.1.1
+# description：
+# 1、需提供当做yum源的iso文件
+# 2、需要提供本机ip地址
+# ###
+
+# 获取iso文件名
+filename=$1
+# 获取本机ip地址
+ip_addr=$2
+# 获取当前路径
+current_path=$(cd "$(dirname $0)";pwd)
+
+################################## 1.生成本地临时仓库 ################################################
+# 挂载到本地目录
+mkdir -p /var/www/html/iso
+mv ./${filename} /var/www/html/
+mount -o loop -t iso9660 /var/www/html/${filename} /var/www/html/iso
+
+# 将原本的所有repo文件备份
+mkdir ${current_path}/repo-back
+mv /etc/yum.repos.d/* ${current_path}/repo-back
+
+# 配置本地repo文件
+cat >/etc/yum.repos.d/iso.repo<< EOF
+[iso]
+name=${filename}
+baseurl=file:///var/www/html/iso
+gpgcheck=0
+enabled=1
+EOF
+
+# 刷新yum缓存
+yum clean all;yum makecache
+
+# 将备份的repo文件移到/etc/yum.repo.d/目录
+mv ${current_path}/repo-back /etc/yum.repos.d/
+
+##################################### 2.制作httpd仓库 ################################################
+# 安装httpd服务
+yum -y install httpd
+
+# 启动httpd服务
+systemctl start httpd
+
+# 将repo文件内容改为http的源路径
+cat >/etc/yum.repos.d/iso.repo<< EOF
+[iso]
+name=${filename}
+baseurl=http://${ip_addr}/iso
+gpgcheck=0
+enabled=1
+EOF
+
+# 刷新yum缓存
+yum clean all;yum makecache
