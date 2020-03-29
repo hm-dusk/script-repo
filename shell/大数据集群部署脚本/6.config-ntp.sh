0fc899de0bc31d710a4b8774ed700f6d9a4476e6
13222 <1322260665@qq.com>
2020-03-29 19:05:45 +0800

    feat(shell): 增加run.sh脚本

--- shell/大数据集群部署脚本/6.config-ntp.sh
+++ shell/大数据集群部署脚本/6.config-ntp.sh
@@ -8,10 +8,11 @@
 # description：
 # 1、需要集群提前做好免密登录（Require the cluster to do secret login in advance）
 # 2、需要集群每个节点配置好离线yum源（Need to configure offline yum source for each node in the cluster）
-# 3、需提供主机名信息文件hostname.txt，格式如下 (Need to provide the hostname information file hostname.txt, the format is as follows)：
+# 3、需要xcall.sh脚本，且放到了/usr/bin/目录下（Requires xcall.sh script and is placed in the / usr / bin / directory）
+# 4、需提供主机名信息文件hostname.txt，格式如下 (Need to provide the hostname information file hostname.txt, the format is as follows)：
 # [ip1] [hostname1] [short hostname1] [password1]
 # [ip2] [hostname2] [short hostname2] [password2]
-# 4、需要2个参数，第1个为hostname.txt文件，第2个为子网掩码（Requires 2 parameters, the first is the hostname.txt file, and the second is the subnet mask）
+# 5、需要2个参数，第1个为hostname.txt文件，第2个为子网掩码（Requires 2 parameters, the first is the hostname.txt file, and the second is the subnet mask）
 # ###
 
 # 获取数据文件名
@@ -27,17 +28,18 @@ gateway_addr=$(ip r | grep default | awk '{print $3}')
 # Get the local IP address
 ip_addr=$(hostname -I | awk '$1=$1')
 
+# 安装ntp服务
+# Install ntp service
+xcall.sh "yum -y install ntp"
+
 # 配置所有节点
 # Configure all nodes
 while read ip host short pwd; do
-ssh -f root@${ip} "yum -y install ntp"
-ssh -f root@${ip} "sed \"12 arestrict ${gateway_addr} mask ${net_mask} nomodify notrap\" -i /etc/ntp.conf"
-ssh -f root@${ip} "sed \"12 arestrict ${ip} nomodify notrap nopeer noquery\" -i /etc/ntp.conf"
+ssh -f root@${ip} "sed \"12 arestrict ${gateway_addr} mask ${net_mask} nomodify notrap\" -i /etc/ntp.conf;sed \"12 arestrict ${ip} nomodify notrap nopeer noquery\" -i /etc/ntp.conf"
 
 ssh -f root@${ip} "sed \"s/server/#server/\" -i /etc/ntp.conf"
 
-ssh -f root@${ip} "sed \"26 aFudge ${ip_addr} stratum 10\" -i /etc/ntp.conf"
-ssh -f root@${ip} "sed \"26 aserver ${ip_addr}\" -i /etc/ntp.conf"
+ssh -f root@${ip} "sed \"26 aFudge ${ip_addr} stratum 10\" -i /etc/ntp.conf;sed \"26 aserver ${ip_addr}\" -i /etc/ntp.conf"
 done < ${filename}
 
 # 修改本机节点为ntp主服务器
---
+++ shell/大数据集群部署脚本/run.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+iso_file_name="CentOS-7-x86_64-Everything-1810.iso"
+host_name_file="hostname.txt"
+net_mask="255.255.255.0"
+mariadb_conf="my.cnf"
+mariadb_root_paaaword="1234"
+mariadb_scm_paaaword="1234"
+mariadb_init_sql="init.sql"
+cm="cm6.3.0"
+cdh="cdh6.3.0"
+
+sh 1.yum-repo.sh ${iso_file_name}
+
+sh 2.ssh-all.sh ${host_name_file}
+
+sh 3.generate-xshell.sh ${host_name_file}
+
+sh 4.yum-repo-all-host.sh
+
+sh 5.init-cdh-env.sh
+
+sh 6.config-ntp.sh ${host_name_file} ${net_mask}
+
+sh 7.install-mariadb.sh ${mariadb_conf} ${mariadb_root_paaaword}
+
+sh 8.init-mariadb.sh ${mariadb_root_paaaword} ${mariadb_init_sql} mysql-connector-java-*.jar
+
+sh 9.cdh-yum-repo.sh ${cm} ${cdh}
+
+sh 10.install-cloudera-manager.sh ${mariadb_scm_paaaword}
